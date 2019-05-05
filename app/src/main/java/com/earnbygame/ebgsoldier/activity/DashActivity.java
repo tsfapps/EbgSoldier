5412d44bfdc90dcc21673058ae95941840ec92c9
tsfapps <appslelo.com@gmail.com>
2019-05-05 18:52:00 +0530

    Side Nav Bar Added

--- app/src/main/java/com/earnbygame/ebgsoldier/activity/DashActivity.java
+++ app/src/main/java/com/earnbygame/ebgsoldier/activity/DashActivity.java
@@ -3,10 +3,7 @@ package com.earnbygame.ebgsoldier.activity;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.FragmentManager;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -117,9 +114,9 @@ public class DashActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_profile) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_wallet) {
 
         } else if (id == R.id.nav_slideshow) {
 
